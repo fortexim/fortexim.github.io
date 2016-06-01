e8261c7a3873dc54c837aadd7e18d5683c91026f
Fberti <norbert.fehervari@gmail.com>
2016-06-01 15:27:08 +0200

    First version with display:table

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// import 'babel-polyfill';
+import world from './world';
+require('../css/bootstrap.css');
+require('../css/app.scss');
+// debugger;
+// document.getElementById('output').innerHTML = `Hello ${world}! from B`;
+
+var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)/-4;
+console.log(w)
+var l = document.getElementsByClassName('innerleft');
+l[0].style.backgroundPositionX = w.toString()+"px";
+console.log("hog")
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+/* global __dirname */
+
+var path = require('path');
+
+var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var dir_js = path.resolve(__dirname, 'js');
+var dir_html = path.resolve(__dirname, 'html');
+var dir_build = path.resolve(__dirname, 'build');
+
+module.exports = {
+    entry: path.resolve(dir_js, 'main.js'),
+    output: {
+        publicPath: 'http://localhost:8080/',
+        path: dir_build,
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        contentBase: dir_build,
+    },
+    module: {
+        loaders: [{
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015']
+            },
+            test: dir_js,
+        }, {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            loader: "style-loader!css-loader"
+        }, {
+            test: /\.scss$/,
+            exclude: /node_modules/,
+            loader: "style-loader!css-loader!sass-loader"
+        }]
+    },
+    plugins: [
+        // Simply copies the files over
+        // new CopyWebpackPlugin([{
+        //         from: dir_html
+        //     } // to: output.path
+        // ]),
+
+        // Avoid publishing files when compilation fails
+        new webpack.NoErrorsPlugin()
+    ],
+    stats: {
+        // Nice colored output
+        colors: true
+    },
+    // Create Sourcemaps for the bundle
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['', '.js', '.es6']
+    },
+};
