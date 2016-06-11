676c75b2de51afde3863606baadf6240bbc7db38
Fberti <norbert.fehervari@gmail.com>
2016-06-12 01:17:46 +0200

    Layouts done.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-require('../css/bootstrap.css');
+require('../css/normalize.css');
 require('../css/app.scss');
+
 
 
 var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)/-4;
 var l = document.getElementsByClassName('innerleft');
-l[0].style.backgroundPositionX = w.toString()+"px";
+// l[0].style.backgroundPositionX = w.toString()+"px";
